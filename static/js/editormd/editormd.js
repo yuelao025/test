76516f8c5129813e977dbe8a9f845814f73f9797
wanmin <wanminny@163.com>
2017-04-21 21:55:09 +0800

    md

--- static/js/editormd/editormd.js
+++ static/js/editormd/editormd.js
@@ -93,7 +93,7 @@
         name                 : "",             // Form element name
         value                : "",             // value for CodeMirror, if mode not gfm/markdown
         theme                : "",             // Editor.md self themes, before v1.5.0 is CodeMirror theme, default empty
-        editorTheme          : "full",      // Editor area, this is CodeMirror theme at v1.5.0
+        editorTheme          : "default",      // Editor area, this is CodeMirror theme at v1.5.0
         previewTheme         : "",             // Preview area theme, default empty
         markdown             : "",             // Markdown source code
         appendMarkdown       : "",             // if in init textarea value not empty, append markdown to textarea
